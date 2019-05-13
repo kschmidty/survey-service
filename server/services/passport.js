5be26b72ae4dbca86f5c8fd2809b58037c71ccc4
Keith Schmidt <k.schmidt@optum.com>
2019-05-13 12:02:51 -0400

    fixed routing within passport

--- server/services/passport.js
+++ server/services/passport.js
@@ -9,7 +9,7 @@ passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: 'auth/google/callback'
+        callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
         console.log('access token', accessToken);
         console.log('refreshToken', refreshToken);
