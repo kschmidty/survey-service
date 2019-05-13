094c16c44612612e46dd493d0d4258eca397bfcb
Keith Schmidt <k.schmidt@optum.com>
2019-05-13 16:15:20 -0400

    fixed formatting

--- server/services/passport.js
+++ server/services/passport.js
@@ -6,23 +6,24 @@ const keys = require("../config/keys");
 const User = mongoose.model("users");
 
 passport.use(
-    new GoogleStrategy({
-            clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: "/auth/google/callback"
-        },
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleId: profile.id }).then(existingUser => {
-                if (existingUser) {
-                    // already have a record with given profile Id
-                    done(null, existingUser);
-                } else {
-                    // don't have a user record, make a new one
-                    new User({ googleId: profile.id })
-                        .save()
-                        .then(user => done(null, user));
-                }
-            });
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // already have a record with given profile Id
+          done(null, existingUser);
+        } else {
+          // don't have a user record, make a new one
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
         }
-    )
+      });
+    }
+  )
 );
